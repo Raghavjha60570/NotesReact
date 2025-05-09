559575c52b6f8711a6428539093452e0c652dc6b
TReX <Rjha60571@gmail.com>
2025-05-09 12:36:24 +0530

    Update and rename ErrorsInNode.txt to ErrorsInNode.jsx

---
+++ ErrorsInNode.jsx
@@ -0,0 +1,5 @@
+<h1>Case: During npm install  npm ENONET error occurs means looking for package.json folder in current branch in terminal<h1>
+-> Just look for subfolder which contains  packge.json  and then move or change your current directory into a subfolder which contain
+  package.json
+
+-> Then run npm install in that working directory which contains package.json
